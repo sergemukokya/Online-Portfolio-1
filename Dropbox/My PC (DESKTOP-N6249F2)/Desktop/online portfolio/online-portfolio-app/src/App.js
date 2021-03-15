8f71fde847baee29cc37b596fd51e5c2a0540917
SERG KM <71732922+sergemukokya@users.noreply.github.com>
2021-03-15 12:22:51 +0200

    Project Setup

--- Dropbox/My PC (DESKTOP-N6249F2)/Desktop/online portfolio/online-portfolio-app/src/App.js
+++ Dropbox/My PC (DESKTOP-N6249F2)/Desktop/online portfolio/online-portfolio-app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <h2>Online Portfolio App</h2>
+    <h2>Online Portfolio App .....</h2>
   );
 }
 
