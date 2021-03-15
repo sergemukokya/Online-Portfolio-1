c16c0f6cd4149daa11b0deaebeef482cfbfeae11
SERG KM <71732922+sergemukokya@users.noreply.github.com>
2021-03-15 16:15:28 +0200

    Navbar Completed

--- Dropbox/My PC (DESKTOP-N6249F2)/Desktop/online portfolio/online-portfolio-app/src/App.js
+++ Dropbox/My PC (DESKTOP-N6249F2)/Desktop/online portfolio/online-portfolio-app/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
 function App() {
   return (
-    <h2>Online Portfolio App .....</h2>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
